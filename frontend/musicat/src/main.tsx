0051da5340fd18e4ac73d48464b1494752177b55
YeonHak Lee <dldusgkr788@gmail.com>
2023-03-14 17:34:15 +0900

    [FEAT] : 프론트 router 스켈레톤 코드 제작중....

--- frontend/musicat/src/main.tsx
+++ frontend/musicat/src/main.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RecoilRoot } from "recoil";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
