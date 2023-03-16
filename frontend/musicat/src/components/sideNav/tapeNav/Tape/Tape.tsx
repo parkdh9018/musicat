9044142c176a4d3452570963a231d1c7e467bd98
parkdh9018 <parkdh9018@naver.com>
2023-03-16 17:53:37 +0900

    [FEAT] : 노래 제목 이동

--- frontend/musicat/src/components/sideNav/tapeNav/Tape/Tape.tsx
+++ frontend/musicat/src/components/sideNav/tapeNav/Tape/Tape.tsx
@@ -3,8 +3,7 @@ import { useEffect } from "react";
 import style from "./Tape.module.css";
 
 export const Tape = () => {
-
-  let an:  anime.AnimeInstance;
+  let an: anime.AnimeInstance;
 
   useEffect(() => {
     an = anime({
@@ -13,6 +12,15 @@ export const Tape = () => {
       easing: "linear",
       loop: true,
       duration: 3000,
+    });
+
+    anime({
+      targets: `.${style.text}`,
+      translateX: 270,
+      duration: 3000,
+      round: 1, // round the text to integer values
+      easing: "linear",
+      loop: true
     });
   }, []);
 
@@ -27,14 +35,20 @@ export const Tape = () => {
   return (
     <>
       <div className={style.tape}>
-        <div className={style.songName}>뉴진스 - ditto</div>
+        <div className={style.songName}>
+          <div className={style.text}>뉴진스 - ditto</div>
+        </div>
         <img className={style.tapeImg} src="/public/img/tape/tape.png" />
         <img className={style.gearImg} style={{top: '110px', left: '103px'}} src="/public/img/tape/gear.png"/>
         <img className={style.gearImg} style={{top: '110px', left: '230px'}} src="/public/img/tape/gear.png"/>
         <img className={style.tapeBackImg} src="/public/img/tape/background_img_test.png"/>
       </div>
+     
       <button onClick={pauseEvent}>pause</button>
       <button onClick={playEvent}>play</button>
+      <div className={style.container}>
+        
+      </div>
     </>
   );
 };
