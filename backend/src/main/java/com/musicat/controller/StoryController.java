dc7d6621a8965306887e3868363aedb7f99b011c
이찬희 <dojsfffff@naver.com>
2023-03-17 13:00:03 +0900

    [FEAT] 사연 신청, 중복 검사, 사연 조회 컨트롤러 수정

--- backend/src/main/java/com/musicat/controller/StoryController.java
+++ backend/src/main/java/com/musicat/controller/StoryController.java
@@ -6,6 +6,8 @@ import com.musicat.service.StoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +29,32 @@ public class StoryController {
             if (storyService.isUniqueStory(storyRequestDto.getMemberSeq())) return ResponseEntity.status(HttpStatus.CONFLICT).body("이미 신청한 사연이 있습니다.");
 
             return ResponseEntity.ok(storyService.insertStory(storyRequestDto));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    /**
+     * 사연 1개 조회 (읽을 사연) (가장 먼저 신청한 사연 중 1개 조회)
+     */
+    @GetMapping("/story")
+    public ResponseEntity<?> getTopStory() {
+        try {
+            return ResponseEntity.ok(storyService.getTopStoryInfo());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    /**
+     * 사연 중복 검사 (신청 사연 리스트에 memberSeq로 신청한 사연이 있는지 여부 판별
+     */
+    @GetMapping("/story/unique/{memberSeq}")
+    public ResponseEntity<Boolean> isUniqueStory(@PathVariable long memberSeq) {
+        try {
+            return ResponseEntity.ok(storyService.isUniqueStory(memberSeq));
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
