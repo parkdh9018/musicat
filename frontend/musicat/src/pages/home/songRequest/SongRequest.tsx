cbdbeb38d23718b3ebcd895d34b056421036d8b8
parkdh9018 <parkdh9018@naver.com>
2023-03-17 16:43:48 +0900

    [FEAT] : 사연 신청 페이지 구성 요소 초기화

--- frontend/musicat/src/pages/home/songRequest/SongRequest.tsx
+++ frontend/musicat/src/pages/home/songRequest/SongRequest.tsx
@@ -1,5 +1,33 @@
-import React from "react";
+import style from "./SongRequest.module.css";
+import { Input } from "@/components/common/input/Input";
+import { Button } from "@/components/common/button/Button";
+import { ContentBox } from "./contentBox/ContentBox";
+import { ContentPlus } from "./contentPlus/ContentPlus";
 
 export const SongRequest = () => {
-  return <div>SongRequest</div>;
+
+  const myInputStyle = {width : '80%', marginLeft: '2%'}
+
+  return (
+    <>
+      <div className={style.songRequest}>
+        <div className={style.group}>
+          제목 <Input style={myInputStyle}/>
+        </div>
+        <div className={style.group}>
+          내용 
+          <div className={style.content} style={{marginLeft: '2%'}}>
+            <ContentBox index={1}/>
+            <ContentPlus/>
+          </div>
+        </div>
+        <div>
+          신청곡 <Input style={myInputStyle}/>
+        </div>
+        <div>
+          <Button content="등록하기" onClick={()=>{}}/>
+        </div>
+      </div>
+    </>
+  )
 };
---
+++ frontend/musicat/src/pages/home/songRequest/contentPlus/ContentPlus.tsx
@@ -0,0 +1,8 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
+
+export const ContentPlus = () => {
+  return (
+    <div>문단 추가하기 <FontAwesomeIcon icon={faCirclePlus}/></div>
+  )
+}
