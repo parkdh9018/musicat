448d9d4cf690761e3c008afc87ce0ff7c8c994ab
2udy <22udy629@gmail.com>
2023-03-17 13:14:07 +0900

    [FEAT]TapeButtons - Button 컴포넌트 적용

--- frontend/musicat/src/components/sideNav/tapeNav/tapeButtons/TapeButtons.tsx
+++ frontend/musicat/src/components/sideNav/tapeNav/tapeButtons/TapeButtons.tsx
@@ -1,33 +1,24 @@
 import style from "./TapeButtons.module.css";
 import { useNavigate } from "react-router-dom";
+import { Button } from "@/components/common/button/Button";
 
 export const TapeButtons = () => {
   const navigate = useNavigate();
 
-  const onClick = (pageAdress: string) => {
-    navigate(`/${pageAdress}`);
-  };
-
   return (
     <div className={style.bg}>
-      <span className={style.play} onClick={onClick("")}>
-        재생
-      </span>
-      <span className={style.stop} onClick={onClick("")}>
-        정지
-      </span>
-      <span className={style.nonClicked} onClick={onClick("")}>
-        채팅
-      </span>
-      <span className={style.clicked} onClick={onClick("story")}>
-        사연
-      </span>
-      <span className={style.nonClicked} onClick={onClick("songRequest")}>
-        노래
-      </span>
-      <span className={style.nonClicked} onClick={onClick("about")}>
-        설명
-      </span>
+      <Button content="재생" onClick={() => {}} className={style.play} />
+      <Button content="정지" onClick={() => {}} className={style.stop} />
+      <Button content="채팅" onClick={() => {}} className={style.unclicked} />
+      <Button
+        content="사연"
+        onClick={() => {
+          navigate("/story");
+        }}
+        className={style.clicked}
+      />
+      <Button content="노래" onClick={() => {}} className={style.unclicked} />
+      <Button content="설명" onClick={() => {}} className={style.unclicked} />
     </div>
   );
 };
