18e59cfc87bb57f01528171e93ace18ac64c8a54
2udy <22udy629@gmail.com>
2023-03-20 10:53:59 +0900

    [FEAT]TapeNav - VolumeBar 컴포넌트 생성

--- frontend/musicat/src/components/sideNav/tapeNav/TapeNav.tsx
+++ frontend/musicat/src/components/sideNav/tapeNav/TapeNav.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { TapeButtons } from "./tapeButtons/TapeButtons";
 import { Tape } from "@/components/sideNav/tapeNav/Tape/Tape";
+import { VolumeBar } from "./volumeBar/VolumeBar";
 
 export const TapeNav = () => {
   return (
     <>
       <TapeButtons />
-      <div>TapeNav</div>;
+      <VolumeBar />
       <Tape />
     </>
   );
