2df50a203eb7a50278f6beba4584e385299ba49f
parkdh9018 <parkdh9018@naver.com>
2023-03-21 15:56:09 +0900

    [FEAT] : 헤더 컴포넌트 구성

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,12 +1,27 @@
 import { userInfoState } from "@/atoms/user.atom";
 import { useRecoilState } from "recoil";
 import style from "./Header.module.css";
+import { OnairSign } from "./onairSign/onairSign";
 
 export const Header = () => {
   const [userInfo, setUserInfo] = useRecoilState(userInfoState);
   return (
     <header className={style.header}>
-      <div className={style.innerHeader}>{userInfo.userNick}</div>
+      <div className={style.innerHeader}>
+        <img className={style.logo_img} src="/img/logo.png" alt="로고"/>
+        <div style={{flexGrow:"1", textAlign:"center"}}>
+          <OnairSign/>
+        </div>
+        <div className={style.user_info}>
+          <span className={style.nickname}>
+            {userInfo.userNick}
+          </span>
+          <span className={style.nickname_other}>
+            님 환영합니다
+          </span>
+          <img src={userInfo.userProfile} alt="프로필 이미지"/>
+        </div>
+      </div>
     </header>
   );
 };
--- frontend/src/components/header/onairSign/onairSign.tsx
+++ frontend/src/components/header/onairSign/onairSign.tsx
@@ -1,5 +1,13 @@
-import React from "react";
+import style from "./OnairSign.module.css";
 
-export const onairSign = () => {
-  return <div>onairSign</div>;
+export const OnairSign = () => {
+  return (
+    <div className={style.onairSign}>
+      <div className={style.logo}>
+        <b>
+          <span>O</span>N A<span>IR</span>
+        </b>
+      </div>
+    </div>
+  );
 };
