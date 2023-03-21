c8ca1f4f586a337ecbb90903e54ffd69079d67e7
parkdh9018 <parkdh9018@naver.com>
2023-03-21 17:10:45 +0900

    [FEAT] : 사용자 프로필 popover

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -2,6 +2,8 @@ import { userInfoState } from "@/atoms/user.atom";
 import { useRecoilState } from "recoil";
 import style from "./Header.module.css";
 import { OnairSign } from "./onairSign/onairSign";
+import { Popover } from "./popover/Popover";
+
 
 export const Header = () => {
   const [userInfo, setUserInfo] = useRecoilState(userInfoState);
@@ -19,8 +21,12 @@ export const Header = () => {
           <span className={style.nickname_other}>
             님 환영합니다
           </span>
+          <div className={style.popover_state}>
+            <Popover/>
+          </div>
           <img src={userInfo.userProfile} alt="프로필 이미지"/>
         </div>
+
       </div>
     </header>
   );
--- frontend/src/components/header/popover/Popover.tsx
+++ frontend/src/components/header/popover/Popover.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import style from "./Popover.module.css";
 
 export const Popover = () => {
-  return <div>Popover</div>;
+  return (
+    <div className={style.popover}>
+      <div className={style.triangle}></div>
+      <div className={style.content}>
+        <Link to="/mypage/myinfo"><div className={style.content_text}>나의 정보 관리</div></Link>
+        <Link to="/mypage/notice"><div className={style.content_text}>알림 / 공지사항</div></Link>
+        <Link to="/mypage/inventory"><div className={style.content_text}>인벤토리</div></Link>
+        {/* TODO : 로그아웃 구현 */}
+        <Link to=""><div className={style.content_text}>로그아웃</div></Link>
+      </div>
+
+    </div>
+  );
 };
