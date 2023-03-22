2b447d66f586033d05b842ea5df2700338b5e16f
parkdh9018 <parkdh9018@naver.com>
2023-03-22 10:46:43 +0900

    [FEAT] : 테이프 nav 수정

--- frontend/src/components/sideNav/tapeNav/TapeNav.tsx
+++ frontend/src/components/sideNav/tapeNav/TapeNav.tsx
@@ -1,14 +1,14 @@
-import React from "react";
 import { TapeButtons } from "./tapeButtons/TapeButtons";
 import { Tape } from "@/components/sideNav/tapeNav/Tape/Tape";
 import { VolumeBar } from "./volumeBar/VolumeBar";
+import style from "./TapeNav.module.css"
 
 export const TapeNav = () => {
   return (
-    <>
+    <div className={style.tapeNav}>
       <TapeButtons />
       <VolumeBar />
       <Tape />
-    </>
+    </div>
   );
 };
