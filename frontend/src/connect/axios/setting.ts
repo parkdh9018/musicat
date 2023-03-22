d5ddfe0e5f995821aca9eeebb9031e81dc730428
YeonHak Lee <dldusgkr788@gmail.com>
2023-03-22 15:25:47 +0900

    [FEAT] 전반적인 css 수정

--- frontend/src/connect/axios/setting.ts
+++ frontend/src/connect/axios/setting.ts
@@ -1,1 +1,14 @@
-export {};
+import axios from "axios";
+
+// axios 객체 생성
+export const $ = axios.create({
+  baseURL: "https://musicat.kr/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// $.interceptors.request.use((config) => {
+//   config.headers["Access-Token"] = sessionStorage.getItem("access-token");
+//   return config;
+// });
