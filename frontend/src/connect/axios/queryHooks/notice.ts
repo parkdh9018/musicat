2c3b612f5b6383ed22369f5243c3f334c6e05c3e
YeonHak Lee <dldusgkr788@gmail.com>
2023-03-24 11:06:10 +0900

    [FEAT] API연결중

--- frontend/src/connect/axios/queryHooks/notice.ts
+++ frontend/src/connect/axios/queryHooks/notice.ts
@@ -1,5 +1,5 @@
 import { $ } from "@/connect/axios/setting";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
 interface Notice {
   noticeSeq: number;
@@ -7,17 +7,6 @@ interface Notice {
   noticeTitle: string;
   // 이거 필요없음
   userNickname: string;
-}
-
-interface Alert {
-  alertSeq: number;
-  // 이거 필요없음
-  userSeq: number;
-  alertTitle: string;
-  // 이거 필요없음
-  alertContent: string;
-  alertCreatedAt: string;
-  alertIsRead: boolean;
 }
 
 // 공지사항 상위 3개를 가져오기
@@ -28,63 +17,4 @@ export function getTop3Notice() {
   }
   const { data, isLoading } = useQuery(["Top3Notice"], fetchTop3Notice);
   return { data, isLoading };
-}
-
-// 알람 list를 받아오기
-export function getAlertList(pageNum: number) {
-  async function fetchAlertList(): Promise<Alert[]> {
-    const { data } = await $.get("/alert/1?page=0&content=");
-    return data.content;
-  }
-  const { data, isLoading } = useQuery(
-    ["AlertListUser", pageNum],
-    fetchAlertList
-  );
-  return { data, isLoading };
-}
-
-// 알람을 읽었다고 optimistic update
-export function temp(pageNum: any, alertSeq: any) {
-  const queryClient = useQueryClient();
-
-  async function fetchAlert() {
-    const { data } = await $.patch(`/alert`, {
-      alertSeq: alertSeq,
-      alertRead: true,
-    });
-    return data;
-  }
-
-  const { mutate: readAlert } = useMutation(fetchAlert, {
-    onMutate: async () => {
-      await queryClient.cancelQueries(["AlertListUser", pageNum]);
-
-      const oldData: Alert[] | undefined = queryClient.getQueryData([
-        "AlertListUser",
-        pageNum,
-      ]);
-
-      queryClient.setQueryData(["AlertListUser", pageNum], (oldData: any) => {
-        const newData = [...oldData];
-
-        newData.forEach((content) => {
-          if (content.alertSeq === alertSeq) {
-            content.alertIsRead = true;
-            return;
-          }
-        });
-
-        return newData;
-      });
-      return { oldData };
-    },
-    onError: (_error, _variables, context) => {
-      queryClient.setQueryData(["AlertListUser", pageNum], context?.oldData);
-    },
-    onSettled: () => {
-      queryClient.invalidateQueries(["AlertListUser", pageNum]);
-    },
-  });
-
-  return readAlert;
 }
--- frontend/src/pages/mypage/notice/Notice.tsx
+++ frontend/src/pages/mypage/notice/Notice.tsx
@@ -5,64 +5,17 @@ import { Pagenation } from "@/components/common/pagenation/Pagenation";
 import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import style from "./Notice.module.css";
 import { useSetRecoilState } from "recoil";
 import { nowSideNavState } from "@/atoms/common.atom";
-import {
-  getAlertList,
-  getTop3Notice,
-  temp,
-} from "@/connect/axios/queryHooks/notice";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { $ } from "@/connect/axios/setting";
+import { getTop3Notice } from "@/connect/axios/queryHooks/notice";
+import { getAlertList } from "@/connect/axios/queryHooks/alert";
+import style from "./Notice.module.css";
 
 export const Notice = () => {
   const [input, setInput] = useState("");
   const setNowSideNav = useSetRecoilState(nowSideNavState);
   const { data: notice } = getTop3Notice();
   const { data: alertList } = getAlertList(1);
-
-  //
-  const queryClient = useQueryClient();
-
-  async function fetchAlert() {
-    const { data } = await $.patch(`/alert`, {
-      alertSeq: 2,
-      alertIsRead: true,
-    });
-    return data;
-  }
-
-  const { mutate: readAlert } = useMutation(fetchAlert, {
-    onMutate: async () => {
-      await queryClient.cancelQueries(["AlertListUser", 1]);
-
-      const oldData = queryClient.getQueryData(["AlertListUser", 1]);
-
-      queryClient.setQueryData(["AlertListUser", 1], (oldData: any) => {
-        const newData = [...oldData];
-
-        newData.forEach((content) => {
-          if (content.alertSeq === 2) {
-            content.alertIsRead = true;
-            return;
-          }
-        });
-        return newData;
-      });
-      return { oldData };
-    },
-    onError: (_error, _variables, context) => {
-      queryClient.setQueryData(["AlertListUser", 1], context?.oldData);
-    },
-    onSettled: () => {
-      queryClient.invalidateQueries(["AlertListUser", 1]);
-    },
-  });
-
-  //
-
-  const aaa = temp(1, 1);
 
   /** 사이드 Nav 초기화 */
   useEffect(() => {
@@ -79,7 +32,7 @@ export const Notice = () => {
       />
       {/* 이 위부분 Board는 파란색으로 칠해야 한다. */}
       <Board
-        data={alertList}
+        data={alertList.content}
         grid={"20% 50% 30%"}
         headRow={[]}
         type={"noticeList"}
@@ -117,8 +70,7 @@ export const Notice = () => {
         <Button
           content="검색"
           onClick={() => {
-            //  readAlert();
-            aaa();
+            return;
           }}
           style={{ margin: "0 5px" }}
         />
