4fef5464c2df793b49267a430571a495c3d27ab4
YeonHak Lee <dldusgkr788@gmail.com>
2023-03-24 15:14:14 +0900

    [FEAT] API 연결중....

--- frontend/src/pages/mypage/noticeDetail/NoticeDetail.tsx
+++ frontend/src/pages/mypage/noticeDetail/NoticeDetail.tsx
@@ -12,22 +12,31 @@ export const NoticeDetail = () => {
   const navigate = useNavigate();
   const userInfo = useRecoilValue(userInfoState);
   const { noticeSeq } = useParams();
-  // const { data: detail } = getAlertDetail(noticeSeq[0] === "n" ? "" : "");
+  const { data: detail } = getAlertDetail(
+    noticeSeq?.charAt(0) === "n"
+      ? `/notice/detail?noticeSeq=${noticeSeq.replace("n", "")}`
+      : `/alert/detail/${noticeSeq}`
+  );
 
   useEffect(() => {
     if (userInfo.userRole === "admin") setNowSideNav("공지사항");
     else setNowSideNav("알림 / 공지사항");
   }, []);
+
   return (
     <div className={style.notice_detail}>
       <hr className={style.bold_hr} style={{ marginTop: "14px" }} />
       <div className={style.title}>
-        <span style={{ textAlign: "center" }}>1</span>
-        <span>사연 당첨을 축하합니다!</span>
-        <span>2022년 12월 14일(수) 13:21</span>
+        <span style={{ textAlign: "center" }}>
+          {detail?.alertSeq || detail?.noticeSeq}
+        </span>
+        <span>{detail?.alertTitle || detail?.noticeTitle}</span>
+        <span>{detail?.alertCreatedAt || detail?.noticeCreatedAt}</span>
       </div>
       <hr className={style.thin_hr} style={{ marginTop: "14px" }} />
-      <div className={style.content}>내용?</div>
+      <div className={style.content}>
+        {detail?.alertContent || detail?.noticeContent}
+      </div>
       <hr className={style.thin_hr} />
       {userInfo.userRole === "admin" ? (
         <div
@@ -40,14 +49,14 @@ export const NoticeDetail = () => {
           <Button
             content="목록으로"
             onClick={() => {
-              navigate("/mypage/notice");
+              navigate(-1);
             }}
           />
           <div style={{ display: "inline-block" }}>
             <Button
               content="수정"
               onClick={() => {
-                navigate(`/mypage/notice-manage/쿼리에서-받아온정보`);
+                navigate(`/mypage/notice-manage/${noticeSeq}`);
               }}
               style={{ marginRight: "10px" }}
             />
@@ -64,7 +73,7 @@ export const NoticeDetail = () => {
           <Button
             content="목록으로"
             onClick={() => {
-              return;
+              navigate(-1);
             }}
           />
         </div>
