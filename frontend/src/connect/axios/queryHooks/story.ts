4089d686a017080e73b6e56bf87b2b793ff6ab66
parkdn9018 <parkdh9018@naver.com>
2023-03-27 21:26:01 +0900

    [FEAT] : 사연 신청 예외 처리

--- frontend/src/connect/axios/queryHooks/story.ts
+++ frontend/src/connect/axios/queryHooks/story.ts
@@ -1,38 +1,52 @@
+import { storyContentState, storyTitleState } from "@/atoms/story.atoms";
 import { $ } from "@/connect/axios/setting";
 import { useCustomToast } from "@/customHooks/useCustomToast";
 import { StoryRequest } from "@/types/home";
-import { useQuery } from "@tanstack/react-query";
 import { AxiosError } from "axios";
-import { toast } from "react-toastify";
+import { useResetRecoilState } from "recoil";
 
 
+const recoilReset = () => {
+    useResetRecoilState(storyTitleState);
+    useResetRecoilState(storyContentState);
+    //TODO : 노래 수정 필요
+    // useResetRecoilState(songTitleState);
+}
 // 사연 신청
-// TODO : 이거 react query로 바꿔야 할듯
-export function postRequestStory(userSeq:number, payload : StoryRequest) {
+export function postRequestStory(userSeq:number, payload : StoryRequest, setButton:React.Dispatch<React.SetStateAction<boolean>>) {
     
     if(!userSeq) {
         useCustomToast("error","로그인이 필요합니다");
     } else if(!payload.storyTitle) {
         useCustomToast("error","제목을 입력해주세요");
-        return;
     } else if (payload.storyContent.length == 0 || payload.storyContent[0].content == "") {
         useCustomToast("error","내용을 확인해주세요");
-        return
     } else if (payload.storyMusicTitle == "") {
         useCustomToast("error","신청곡을 확인해주세요");
-        return;
+    } else {
+        $.post("/story", payload).then(() => {
+            useCustomToast("success", "사연이 신청되었습니다");
+            setButton(false);
+            recoilReset();
+        }).catch((e) => {
+            useCustomToast("error", e.reponse.data);
+        });
     }
-    $.post("/story", payload).then(() => {
-        useCustomToast("success", "사연이 신청되었습니다");
-    }).catch((e) => {
-        useCustomToast("error", e.reponse.data);
-    });
+
 }
 
-export function isAlreadyReqeustStory(seq: number) {
-    $.get(`/story/unique/${seq}`).then((data) => {
-        // console.log(data)
-    }).catch((e:AxiosError) => {
-        useCustomToast("error", e.response?.data as string);
-    })
+export function isAlreadyReqeustStory(userSeq: number, setButton:React.Dispatch<React.SetStateAction<boolean>>) {
+
+    if(!userSeq) {
+        useCustomToast("error","로그인이 필요합니다");
+        setButton(false);
+
+    } else {
+        $.get(`/story/unique/${userSeq}`).then((data) => {
+            // console.log(data)
+            setButton(false);
+        }).catch((e:AxiosError) => {
+            useCustomToast("error", e.response?.data as string);
+        })
+    }
 }
