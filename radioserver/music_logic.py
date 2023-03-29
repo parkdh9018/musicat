97ee86cd27febbbc980daf5abcff92ec4cc48fe1
42.4.woonchoi <42.4.woonchoi@gmail.com>
2023-03-29 16:46:13 +0900

    [FIX] 남아있는 데이터 처리 로직 추가 (형식이 다른 이슈)

--- radioserver/music_logic.py
+++ radioserver/music_logic.py
@@ -8,6 +8,18 @@ from my_util import create_mp3_url
 
 
 async def process_music_data(data):
+    music_seq = int(data["musicSeq"])
+    print(f'[Music] : GPT 응답 생성중 (musicSeq = {music_seq})')
+    user_nickname = database.find_user_nickname(data["userSeq"])
+    artist = data["musicArtist"]
+    title = data["musicTitle"]
+    release_date = data["musicReleaseDate"]
+    music_intro = chatgpt.music_intro_gpt(artist, title, release_date)
+    music_outro = chatgpt.music_outro_gpt(artist, title, user_nickname)
+    database.update_intro_outro(music_seq, music_intro, music_outro)
+    print(f'[Music] : GPT 응답 생성 완료 (musicSeq = {music_seq})')
+
+async def process_remain_music_data(data):
     music_seq = int(data["music_seq"])
     print(f'[Music] : GPT 응답 생성중 (musicSeq = {music_seq})')
     user_nickname = database.find_user_nickname(data["user_seq"])
@@ -18,7 +30,6 @@ async def process_music_data(data):
     music_outro = chatgpt.music_outro_gpt(artist, title, user_nickname)
     database.update_intro_outro(music_seq, music_intro, music_outro)
     print(f'[Music] : GPT 응답 생성 완료 (musicSeq = {music_seq})')
-
 
 async def process_music_state():
     music = database.find_oldest_unplayed_music()
