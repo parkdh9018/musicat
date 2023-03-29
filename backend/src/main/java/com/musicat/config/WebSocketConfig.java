a7acb4211054a67d95b52c04c31ebc911bc0ba69
이찬희 <dojsfffff@naver.com>
2023-03-29 14:22:05 +0900

    [CHORE] 웹소켓 CORS 설정 변경

--- backend/src/main/java/com/musicat/config/WebSocketConfig.java
+++ backend/src/main/java/com/musicat/config/WebSocketConfig.java
@@ -24,7 +24,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*") // 프론트엔드 주소를 허용하도록 설정
-                .addInterceptors(new HttpSessionIdHandshakeInterceptor())
+//                .addInterceptors(new HttpSessionIdHandshakeInterceptor())
                 .withSockJS();
     }
 
