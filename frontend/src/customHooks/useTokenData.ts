fc105eff62f5286adb90688130bc316b0afa3334
YeonHak Lee <dldusgkr788@gmail.com>
2023-03-29 21:41:55 +0900

    [FIX] 로그아웃시 소캣통신 버그 수정

--- frontend/src/customHooks/useTokenData.ts
+++ frontend/src/customHooks/useTokenData.ts
@@ -1,7 +1,8 @@
 import { Buffer } from "buffer";
 
 export function useTokenData() {
-  const token = localStorage.getItem("token") || "null";
+  const token = localStorage.getItem("token");
+  if (!token) return undefined;
   const base64Payload = token.split(".")[1];
   const payload = Buffer.from(base64Payload, "base64");
   const result = JSON.parse(payload.toString());
