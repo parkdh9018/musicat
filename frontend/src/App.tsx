fea1e88adb1c241a1368e14686a95fdc9d8d6bf5
YeonHak Lee <dldusgkr788@gmail.com>
2023-03-29 17:56:48 +0900

    [FEAT] 채팅 구현80%즈음

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,11 @@ import { useRoutes } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import router from "./pages/router";
-
 import { ToastContainer } from "react-toastify";
-import { useEffect } from "react";
 
 function App() {
   const element = useRoutes(router);
+
   return (
     <div className="App">
       {element}
--- frontend/src/pages/home/chat/Chat.tsx
+++ frontend/src/pages/home/chat/Chat.tsx
@@ -1,67 +1,90 @@
 import { Button } from "@/components/common/button/Button";
 import { Input } from "@/components/common/input/Input";
-import { Pagenation } from "@/components/common/pagenation/Pagenation";
-import { SelectBox } from "@/components/common/selectBox/SelectBox";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import SockJS from "sockjs-client";
+import Stomp from "stompjs";
+import style from "./Chat.module.css";
+import { useTokenData } from "@/customHooks/useTokenData";
+import { submit } from "@/connect/socket/socket.chat";
 
 export const Chat = () => {
-  const [test, setTest] = useState("");
+  // const sockJS = new SockJS("https://musicat.kr/api/ws");
+  const sockJS = new SockJS("http://70.12.246.161:9999/ws");
+  const stompClient = Stomp.over(sockJS);
 
-  const dumyOption = [
-    { value: "none", name: "성별" },
-    { value: "male", name: "남성as" },
-    { value: "female", name: "여성" },
-  ];
+  // 채팅 메세지, 출력될 리스트, 나의 정보
+  const [message, setMessage] = useState<string>("");
+  const [msgList, setMsgList] = useState<string[]>([]);
+  const messageBoxRef = useRef<HTMLDivElement>(null);
+  const userInfo = useTokenData();
 
-  const dumyData = [
-    { a: 213, b: 1234, c: 123451 },
-    { a: 213, b: 1234, c: 12345 },
-    { a: 213, b: 1234, c: 12345 },
-    { a: 213, b: 1234, c: 12345 },
-    { a: 213, b: 1234, c: 12345 },
-    { a: 213, b: 1234, c: 12345 },
-    { a: 213, b: 1234, c: 12345 },
-    { a: 213, b: 1234, c: 12345 },
-    { a: 213, b: 1234, c: 12345 },
-    { a: 213, b: 1234, c: 12345 },
-    { a: 213, b: 1234, c: 12345 },
-    { a: 213, b: 1234, c: 12345 },
-  ];
+  /** 스크롤을 맨 밑으로 고정 */
+  const scrollToBottom = () => {
+    if (messageBoxRef.current) {
+      messageBoxRef.current.scrollTop = messageBoxRef.current.scrollHeight;
+    }
+  };
 
-  const [selectdumy, setSelectdumy] = useState("none");
-  const [isModalOpen, setModal] = useState(false);
+  useEffect(() => {
+    stompClient.connect({}, () => {
+      console.log("websocket connect");
+
+      /** 다른 사람이 채팅을 치면 일어날 일 */
+      stompClient.subscribe(`/topic/messages`, (data) => {
+        const newMessage = JSON.parse(data.body);
+
+        // 내가 보낸 메시지라면
+        console.log(typeof newMessage.senderSeq);
+        console.log(typeof userInfo.userSeq);
+        console.log(newMessage.senderSeq);
+        console.log(userInfo.userSeq);
+        if (newMessage.senderSeq === userInfo.userSeq) {
+          console.log("내가보낸 매세지임");
+          setMsgList((prev) => [
+            ...prev,
+            `${newMessage.sender} : ${newMessage.content}`,
+          ]);
+        }
+        // 다른 사람이 보낸 메시지라면
+        else {
+          console.log("다른사람 메세지임");
+          setMsgList((prev) => [
+            ...prev,
+            `${newMessage.sender} : ${newMessage.content}`,
+          ]);
+        }
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [msgList]);
 
   return (
-    <div style={{ minHeight: "360px", height: "auto" }}>
-      Chat
-      <Button
-        content={"강rkd"}
-        onClick={() => {
-          return;
-        }}
-        style={{
-          borderRadius: "100px",
-          fontSize: "16px",
-          paddingRight: "20px",
-          paddingLeft: "20px",
-        }}
-      />
-      <button onClick={() => setModal(true)}>모달열기</button>
-      <Input
-        input=""
-        placeholder={"이거 placeholder 들어감"}
-        type={test}
-        setInput={setTest}
-      />
-      <SelectBox options={dumyOption} setValue={setSelectdumy} />
-      <Pagenation
-        number={5}
-        first={false}
-        last={false}
-        totalPages={14}
-        url={""}
-      />
-      <button onClick={() => setModal(true)}>모달열기</button>
+    <div className={style.chat_component}>
+      <div className={style.chat_list} ref={messageBoxRef}>
+        {msgList.map((content) => {
+          return <p key={uuidv4()}>{content}</p>;
+        })}
+      </div>
+      <hr className={style.hr} />
+      <div className={style.input_area}>
+        <Input
+          input={message}
+          setInput={setMessage}
+          style={{ width: "80%" }}
+          onKeyDown={() => submit(stompClient, userInfo, setMessage, message)}
+        />
+        <Button
+          content="전 송"
+          style={{ marginLeft: "10px" }}
+          onClick={() => {
+            submit(stompClient, userInfo, setMessage, message);
+          }}
+        />
+      </div>
     </div>
   );
 };
