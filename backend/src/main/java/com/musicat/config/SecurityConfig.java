4cfd020fb5331d5c0a56bc18bf76c0b6908d024b
cladren123 <cladren12332@naver.com>
2023-03-29 15:33:32 +0900

    [FIX] db 생성

--- backend/src/main/java/com/musicat/config/SecurityConfig.java
+++ backend/src/main/java/com/musicat/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
 
     /*
     webSecurityCustomizer 인터페이스의 customize 메소드를 구현하면 WebSecurity 객체를 사용해 보안 설정을 변경할 수 있다.
-    이 코드에서는 web 이라는 람다 매개변수를 사용해 WebSecurity 객체에 접근합니다.
+    이 코드에서는 web 이라는 람다 매개변수를 사용해 WebSecurity 객체에 접근합니다...
 
     web.ignoring().antMatchers() 메소드를 사용하면 Spring Security 가 특정 URL 패턴에 대한 요청을 무시하도록 설정할 수 있습니다.
     무시하도록 설정하면 이에 대한 요청은 Spring Security에 의한 인증 및 인가 처리를 거치지 않아도 된다.
