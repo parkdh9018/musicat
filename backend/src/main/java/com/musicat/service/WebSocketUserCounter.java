7c9741a3afcbf049565e0d1a393455d7fb246346
이찬희 <dojsfffff@naver.com>
2023-03-30 17:44:36 +0900

    [FEAT] 채팅 소켓 통신 접속한 사용자 수 변경 감지 로직, REST API 구현

--- backend/src/main/java/com/musicat/service/WebSocketUserCounter.java
+++ backend/src/main/java/com/musicat/service/WebSocketUserCounter.java
@@ -1,24 +1,24 @@
-//package com.musicat.service;
-//
-//import java.util.concurrent.atomic.AtomicInteger;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class WebSocketUserCounter {
-//
-//    private AtomicInteger userCount = new AtomicInteger(0);
-//
-//    public void increment() {
-//        userCount.incrementAndGet();
-//    }
-//
-//    public void decrement() {
-//        userCount.decrementAndGet();
-//    }
-//
-//    public int getCount() {
-//        return userCount.get();
-//    }
-//
-//
-//}
+package com.musicat.service;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebSocketUserCounter {
+
+    private AtomicInteger userCount = new AtomicInteger(0);
+
+    public void increment() {
+        userCount.incrementAndGet();
+    }
+
+    public void decrement() {
+        userCount.decrementAndGet();
+    }
+
+    public int getCount() {
+        return userCount.get();
+    }
+
+
+}
