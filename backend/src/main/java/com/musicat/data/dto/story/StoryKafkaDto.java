260b1d52ecf208d8bd5b502c310e75c0494989b6
이찬희 <dojsfffff@naver.com>
2023-03-30 15:35:08 +0900

    [FIX] 사연 -> 카프카 DTO 수정

---
+++ backend/src/main/java/com/musicat/data/dto/story/StoryKafkaDto.java
@@ -0,0 +1,22 @@
+package com.musicat.data.dto.story;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class StoryKafkaDto {
+
+    private long storySeq;
+
+    private long userSeq;
+
+    private String storyTitle;
+
+    private String storyContent;
+
+    private String storyMusicTitle;
+
+    private String storyMusicArtist;
+
+}
