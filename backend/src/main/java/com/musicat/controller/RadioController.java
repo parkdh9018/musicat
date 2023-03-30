f5cd83a3b2b24ffc42be28ac1eab79dc9387b762
42.4.woonchoi <42.4.woonchoi@gmail.com>
2023-03-30 16:50:44 +0900

    [FIX] : radio socket subscribe 관련 로직 수정

--- backend/src/main/java/com/musicat/controller/RadioController.java
+++ backend/src/main/java/com/musicat/controller/RadioController.java
@@ -4,9 +4,11 @@ import com.musicat.data.dto.CurrentSoundDto;
 import com.musicat.data.dto.SocketBaseDto;
 import com.musicat.service.RadioService;
 import com.musicat.service.StoryService;
+import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
@@ -18,15 +20,16 @@ public class RadioController {
   private static final Logger logger = LoggerFactory.getLogger(RadioController.class);
   private final RadioService radioService;
 
-  @SubscribeMapping("/topic")
-  public SocketBaseDto<CurrentSoundDto> subscribeToRadio() {
+  private final SimpMessagingTemplate simpMessagingTemplate;
+
+  @MessageMapping("/subscribe")
+  public void subscribeToRadio(Principal principal) {
     CurrentSoundDto currentSound = radioService.getCurrentSound();
-    logger.debug("라디오 소켓 연결됨. 현재 재생중인 음원 정보 : {}", currentSound);
     SocketBaseDto<CurrentSoundDto> socketBaseDto = SocketBaseDto.<CurrentSoundDto>builder()
         .type("radio")
         .operation(radioService.getCurrentState())
         .data(currentSound)
         .build();
-    return socketBaseDto;
+    simpMessagingTemplate.convertAndSendToUser(principal.getName(), "/queue", socketBaseDto);
   }
 }
