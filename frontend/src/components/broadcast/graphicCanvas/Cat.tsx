72ce26185539707f66d23b04e2d898470e42c78b
parkdh9018 <parkdh9018@naver.com>
2023-03-30 09:24:05 +0900

    [FEAT] : 고양이 애니메이션 추가

--- frontend/src/components/broadcast/graphicCanvas/Cat.tsx
+++ frontend/src/components/broadcast/graphicCanvas/Cat.tsx
@@ -17,9 +17,20 @@ export const Cat = (props: propsType) => {
     "/broadcast/cat/Chibi_Cat_01.glb"
   ) as ExtendedGLTF;
 
+  const { animations } = useGLTF(
+    "broadcast/cat/anim/Anim_Chibi@Victory.glb"
+  ) as ExtendedGLTF;
+
+  const { ref, actions, names } = useAnimations(animations);
+
+
+  useEffect(() => {
+    actions[names[0]]?.play();
+  }, [actions]);
+
 
   return (
-    <group {...props}>
+    <group {...props} ref={ref as any}>
       <primitive object={scene} />
     </group>
   );
