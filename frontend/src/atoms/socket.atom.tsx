ab01547fa0d49fdd90e5f6efda76e4f78d6b46d7
parkdh9018 <parkdh9018@naver.com>
2023-03-30 15:05:14 +0900

    [FEAT] : socket 통신 recoil 초기 세팅

--- frontend/src/atoms/socket.atom.tsx
+++ frontend/src/atoms/socket.atom.tsx
@@ -1,6 +1,60 @@
 import { ReactElement } from "react";
-import { atom } from "recoil";
+import { atom, selector, useRecoilCallback } from "recoil";
 import { v4 as uuidv4 } from "uuid";
+import SockJS from "sockjs-client";
+import Stomp from "stompjs";
+
+interface SendDataParams {
+  api: string;
+  data: object;
+}
+
+const SOCKET_BASE_URL = "http://70.12.246.161:9999/ws";
+
+let stompClient: Stomp.Client | null = null;
+
+// 소켓 연결 상태를 관리하는 atom
+export const socketConnectionState = atom({
+  key: "socketConnectionState",
+  default: false,
+});
+
+// 소켓 통신에서 받는 데이터를 저장하는 atom
+export const receivedData = atom({
+  key: "receivedData",
+  default: null,
+});
+
+// 소켓 연결 및 해제 로직을 관리하는 selector
+export const socketConnection = () => {
+  const callback = useRecoilCallback(({ set }) => async () => {
+    const socket = new SockJS(SOCKET_BASE_URL);
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, () => {
+      stompClient?.subscribe('/topic', (message) => {
+        set(receivedData, JSON.parse(message.body));
+      });
+      stompClient?.subscribe('/user/queue', (message) => {
+        set(receivedData, JSON.parse(message.body));
+      });
+    });
+  });
+
+  return callback;
+} 
+
+// 데이터 전송
+export const sendData = async (params: SendDataParams) => {
+  const { data, api } = params;
+
+  if (stompClient && stompClient.connected) {
+    stompClient.send(api, {}, JSON.stringify(data));
+  } else {
+    console.error("소켓이 연결되지 않았습니다.");
+  }
+};
+
+//////////////////////////////////////////////////////////////////
 
 // 채팅 List
 export const chatListState = atom<ReactElement[]>({
--- frontend/src/pages/home/chat/Chat.tsx
+++ frontend/src/pages/home/chat/Chat.tsx
@@ -4,16 +4,35 @@ import { useEffect, useRef, useState } from "react";
 import style from "./Chat.module.css";
 import { useTokenData } from "@/customHooks/useTokenData";
 import { submit } from "@/connect/socket/socket.chat";
-import { useRecoilState } from "recoil";
-import { changeChatList, chatListState } from "@/atoms/socket.atom";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { changeChatList, chatListState, receivedData, sendData, socketConnection, socketConnectionState } from "@/atoms/socket.atom";
 import SocketManager from "@/connect/socket/socket";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUsers } from "@fortawesome/free-solid-svg-icons";
 
 export const Chat = () => {
   // 싱글톤 웹소켓 객채를 소환
-  const socketManager = SocketManager.getInstance();
-  const stompClient = socketManager.connect();
+  // const socketManager = SocketManager.getInstance();
+  // const stompClient = socketManager.connect();
+
+  // const [connected, setConnected] = useRecoilState(socketConnectionState); 
+  // const connectOrDisconnect = useRecoilState(socketConnection)[1];
+  const data = useRecoilValue(receivedData);
+
+  const socket = socketConnection();
+
+  useEffect(() => {
+    if(data) {
+      console.log("받음 : ", data)
+    }
+  },[data]);
+
+  useEffect(() => {
+    socket();
+  },[])
+
+
+
   const userInfo = useTokenData();
 
   // 채팅 메세지, 출력될 리스트, 나의 정보
@@ -28,37 +47,37 @@ export const Chat = () => {
     }
   };
 
-  useEffect(() => {
-    if (!userInfo) return;
+  // useEffect(() => {
+  //   if (!userInfo) return;
 
-    stompClient.connect({}, () => {
-      console.log("websocket connect");
+  //   stompClient.connect({}, () => {
+  //     console.log("websocket connect");
 
-      /** 다른 사람이 채팅을 치면 일어날 일 */
-      stompClient.subscribe(`/topic/messages`, (data) => {
-        const newMessage = JSON.parse(data.body);
+  //     /** 다른 사람이 채팅을 치면 일어날 일 */
+  //     stompClient.subscribe(`/topic/messages`, (data) => {
+  //       const newMessage = JSON.parse(data.body);
 
-        // 여기서는 newMessage를 파라미터로 넣어서 로직 함수를 집어넣으면 좋을 것 같다.
-        // 근데 이부분은 백앤드랑 주고받는 데이터 종류를 확정하는게 필요
+  //       // 여기서는 newMessage를 파라미터로 넣어서 로직 함수를 집어넣으면 좋을 것 같다.
+  //       // 근데 이부분은 백앤드랑 주고받는 데이터 종류를 확정하는게 필요
 
-        // 내가 보낸 메시지라면
-        if (newMessage.senderSeq === Number(userInfo.userSeq)) {
-          console.log("내가보낸 매세지임");
-          setChatList((prev) =>
-            changeChatList(prev, `${newMessage.sender} : ${newMessage.content}`)
-          );
-        }
+  //       // 내가 보낸 메시지라면
+  //       if (newMessage.senderSeq === Number(userInfo.userSeq)) {
+  //         console.log("내가보낸 매세지임");
+  //         setChatList((prev) =>
+  //           changeChatList(prev, `${newMessage.sender} : ${newMessage.content}`)
+  //         );
+  //       }
 
-        // 다른 사람이 보낸 메시지라면
-        else {
-          console.log("다른사람 메세지임");
-          setChatList((prev) =>
-            changeChatList(prev, `${newMessage.sender} : ${newMessage.content}`)
-          );
-        }
-      });
-    });
-  }, []);
+  //       // 다른 사람이 보낸 메시지라면
+  //       else {
+  //         console.log("다른사람 메세지임");
+  //         setChatList((prev) =>
+  //           changeChatList(prev, `${newMessage.sender} : ${newMessage.content}`)
+  //         );
+  //       }
+  //     });
+  //   });
+  // }, []);
 
   // 채팅창을 맨 아래로 유지시킴
   useEffect(() => {
@@ -82,19 +101,26 @@ export const Chat = () => {
           input={message}
           setInput={setMessage}
           style={{ width: "80%" }}
-          onKeyDown={() =>
-            userInfo && stompClient
-              ? submit(stompClient, userInfo, setMessage, message)
-              : undefined
+          onKeyDown={() => {}
+            // userInfo && stompClient
+            //   ? submit(stompClient, userInfo, setMessage, message)
+            //   : undefined
           }
         />
         <Button
           content="전 송"
           style={{ marginLeft: "10px" }}
           onClick={() => {
-            userInfo && stompClient
-              ? submit(stompClient, userInfo, setMessage, message)
-              : undefined;
+            sendData({api: "/app/chat", data : { 
+              senderSeq:2,
+              sender: "paff",
+              content: "안녕"
+            
+            }})
+
+            // userInfo && stompClient
+            //   ? submit(stompClient, userInfo, setMessage, message)
+            //   : undefined;
           }}
         />
       </div>
