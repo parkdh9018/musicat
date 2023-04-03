4b0d2caf8319d42228ac1ae2ef0133170d01bae3
parkdh9018 <parkdh9018@naver.com>
2023-04-03 11:03:03 +0900

    [FIX] : ON AIR 상태 한글로

--- frontend/src/components/header/onairSign/onairSign.tsx
+++ frontend/src/components/header/onairSign/onairSign.tsx
@@ -1,21 +1,33 @@
 import { useRecoilValue } from "recoil";
 import style from "./OnairSign.module.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { broadcastOperationState } from "@/atoms/broadcast.atom";
 
 export const OnairSign = () => {
   const broadcastOperation = useRecoilValue(broadcastOperationState);
 
-  useEffect(() => {}, [broadcastOperation]);
+  const [title, setTitle] = useState("방송중");
+
+  useEffect(() => {
+    if (broadcastOperation == "IDLE") {
+      setTitle("휴식중");
+    } else if (broadcastOperation == "CHAT") {
+      setTitle("소통중");
+    } else if (broadcastOperation == "MUSIC") {
+      setTitle("음악감상중");
+    } else if (broadcastOperation == "STORY") {
+      setTitle("사연소개중")
+    }
+  }, [broadcastOperation]);
 
   return (
     <div className={style.onairSign}>
       <div className={style.box}>
         <div className={style.logo}>
           <b>
-            <span>{broadcastOperation.slice(0, 1)}</span>
-            <span>{broadcastOperation.slice(1, 2)}</span>
-            <span>{broadcastOperation.slice(2, 10)}</span>
+            <span>{title.slice(0, 1)}</span>
+            <span>{title.slice(1, 2)}</span>
+            <span>{title.slice(2, 10)}</span>
             {/* <span>O</span>N A<span>IR</span> */}
           </b>
         </div>
