c728c6d16b79a2ed0e393ef74d11b052ac9578ef
parkdh9018 <parkdh9018@naver.com>
2023-04-03 10:52:13 +0900

    [FEAT] : 배경 수정 + 인벤토리 배경 수정

--- frontend/src/components/broadcast/graphicCanvas/GraphicCanvas.tsx
+++ frontend/src/components/broadcast/graphicCanvas/GraphicCanvas.tsx
@@ -11,15 +11,9 @@ export const GraphicCanvas = () => {
   return (
     <Canvas camera={{ fov: 50, near: 0.1, far: 500, position: [-1.34, 0.95, 1.70], scale:1 }}> 
       <Cat scale={0.5}  position={new THREE.Vector3(0, 0.1, -0.7)}/>
-
       <Background />
       <pointLight intensity={0.3} position={[0,1,0]}/>
       <ambientLight intensity={0.13} />
-      {/* <Whirligig position={[0,-30,-100]} scale={1}/> */}
-
-      {/* <OrthographicCamera />*/}
-      {/* <PerspectiveCamera position={[-1.991, 1.340,1.789]} rotation={[-36.05, -42.34, -26.11]} fov={50} far={1000} near={0.01}/> */}
-      {/* <OrbitControls /> */}
     </Canvas>
   );
 };
