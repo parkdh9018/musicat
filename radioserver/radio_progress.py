178ca7d4a58f35c70a06cb243f5560d599930b9c
42.4.woonchoi <42.4.woonchoi@gmail.com>
2023-04-03 09:38:23 +0900

    [FIX] 음성 파일 무한 생성 방지 (100개)

--- radioserver/radio_progress.py
+++ radioserver/radio_progress.py
@@ -1,9 +1,10 @@
 from collections import deque
-from logic_chat import user_check
+from logic_chat import user_check, count
 from shared_state import current_state, chat_readable
 import kafka_handler
 import logic_music, logic_story
 from my_logger import setup_logger
+
 
 logger = setup_logger()
 
@@ -26,6 +27,7 @@ async def radio_progress():
         radio_state = await logic_story.process_story_state()
     elif current_state.get_state() == 'chat':
         radio_state = {"state" : "chat"}
+        count = 1
         user_check.clear()
         chat_readable.set_state(True)
     elif current_state.get_state() == 'music':
