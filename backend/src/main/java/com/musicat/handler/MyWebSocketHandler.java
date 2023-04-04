a8332d2cb40e241fc32e4250e159ede9466083a9
이찬희 <dojsfffff@naver.com>
2023-04-04 15:18:54 +0900

    [FIX] 웺 소켓 사용자 연결 수 감지 로직 수정

--- backend/src/main/java/com/musicat/handler/MyWebSocketHandler.java
+++ backend/src/main/java/com/musicat/handler/MyWebSocketHandler.java
@@ -1,27 +1,39 @@
-//package com.musicat.handler;
-//
-//import java.util.concurrent.ConcurrentHashMap;
-//import org.springframework.web.socket.CloseStatus;
-//import org.springframework.web.socket.WebSocketSession;
-//import org.springframework.web.socket.handler.TextWebSocketHandler;
-//
-//public class MyWebSocketHandler extends TextWebSocketHandler {
-//  private ConcurrentHashMap<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
-//
-//  @Override
-//  public void afterConnectionEstablished(WebSocketSession session) {
-//    sessions.put(session.getId(), session);
-//    System.out.println("현재 접속자 수: " + sessions.size());
-//  }
-//
-//  @Override
-//  public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
-//    sessions.remove(session.getId());
-//    System.out.println("현재 접속자 수: " + sessions.size());
-//  }
-//
-//  public int getConnectedUsersCount() {
-//    return sessions.size();
-//  }
-//
-//}
+package com.musicat.handler;
+
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketSession;
+
+@Component
+public class MyWebSocketHandler implements StompMessageHandler {
+
+  private List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+  @Override
+  public void afterConnected(WebSocketSession session, StompHeaderAccessor accessor) {
+    sessions.add(session);
+  }
+
+  @Override
+  public void handleFrame(WebSocketSession session, StompHeaderAccessor accessor, Object payload) {
+    // Handle incoming messages
+  }
+
+  @Override
+  public void handleException(WebSocketSession session, StompCommand command, StompHeaderAccessor accessor, byte[] payload, Throwable exception) {
+    // Handle exceptions
+  }
+
+  @Override
+  public void handleTransportError(WebSocketSession session, Throwable exception) {
+    // Handle transport errors
+  }
+
+  public int getSessionCount() {
+    return sessions.size();
+  }
+}
