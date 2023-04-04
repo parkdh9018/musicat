0c8e9042648dae9da32a8af868df9adee168b337
이찬희 <dojsfffff@naver.com>
2023-04-04 15:30:23 +0900

    [FIX] 웹 소켓 연결 수 로직 테스트

--- backend/src/main/java/com/musicat/handler/MyWebSocketHandler.java
+++ backend/src/main/java/com/musicat/handler/MyWebSocketHandler.java
@@ -3,34 +3,41 @@ package com.musicat.handler;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
-import org.springframework.messaging.simp.stomp.StompCommand;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 
 @Component
-public class MyWebSocketHandler implements StompMessageHandler {
+public class MyWebSocketHandler implements WebSocketHandler {
 
   private List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
 
   @Override
-  public void afterConnected(WebSocketSession session, StompHeaderAccessor accessor) {
+  public void afterConnectionEstablished(WebSocketSession session) throws Exception {
     sessions.add(session);
   }
 
   @Override
-  public void handleFrame(WebSocketSession session, StompHeaderAccessor accessor, Object payload) {
-    // Handle incoming messages
+  public void handleMessage(WebSocketSession session, WebSocketMessage<?> message)
+      throws Exception {
+
   }
 
   @Override
-  public void handleException(WebSocketSession session, StompCommand command, StompHeaderAccessor accessor, byte[] payload, Throwable exception) {
-    // Handle exceptions
+  public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+    sessions.remove(session);
   }
 
   @Override
-  public void handleTransportError(WebSocketSession session, Throwable exception) {
+  public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
     // Handle transport errors
+  }
+
+  @Override
+  public boolean supportsPartialMessages() {
+    return false;
   }
 
   public int getSessionCount() {
