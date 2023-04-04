bde5a82f437cebc2be4f3648551426611de325b2
YeonHak Lee <dldusgkr788@gmail.com>
2023-04-04 16:45:46 +0900

    [FEAT] 미니 방송 / 재생 / 정지

--- frontend/src/components/broadcast/Broadcast.tsx
+++ frontend/src/components/broadcast/Broadcast.tsx
@@ -1,17 +1,26 @@
-import { useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue, useResetRecoilState } from "recoil";
 import { GraphicCanvas } from "./graphicCanvas/GraphicCanvas";
 import { RadioPlayer } from "./radioPlayer/RadioPlayer";
 import { socketConnection } from "@/atoms/socket.atom";
 import { nowMainPageState } from "@/atoms/common.atom";
 import { useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlayCircle,
+  faStopCircle,
+} from "@fortawesome/free-regular-svg-icons";
 import { playNowState } from "@/atoms/song.atom";
 import style from "./Broadcast.module.css";
 import radioBoothIMG from "@/asset/img/radioBooth.png";
+import SocketManager from "@/connect/socket/socket";
+import { musicState } from "@/atoms/song.atom";
 
 export const Broadcast = () => {
   const nowMainPage = useRecoilValue(nowMainPageState);
-  const playNow = useRecoilValue(playNowState);
+  const [playNow, setPlayNow] = useRecoilState(playNowState);
   const socket = socketConnection();
+  const stopRadio = useResetRecoilState(musicState);
+  const socketManager = SocketManager.getInstance();
 
   useEffect(() => {
     if (playNow) socket();
@@ -23,7 +32,35 @@ export const Broadcast = () => {
         nowMainPage ? style.broadcast : style.broadcast + " " + style.mypage
       }
     >
-      <img src={radioBoothIMG} alt="" className={style.radioBooth} />
+      <img
+        src={radioBoothIMG}
+        alt=""
+        className={style.radioBooth}
+        style={!nowMainPage ? { display: "none" } : undefined}
+      />
+      <div
+        className={style.broad_back}
+        style={nowMainPage ? { display: "none" } : undefined}
+      >
+        <FontAwesomeIcon
+          icon={faPlayCircle}
+          className={style.play}
+          style={!playNow ? undefined : { display: "none" }}
+          onClick={() => {
+            setPlayNow(true);
+          }}
+        />
+        <FontAwesomeIcon
+          icon={faStopCircle}
+          className={style.play}
+          style={playNow ? undefined : { display: "none" }}
+          onClick={() => {
+            setPlayNow(false);
+            stopRadio();
+            socketManager.disconnect();
+          }}
+        />
+      </div>
       <GraphicCanvas />
       <RadioPlayer />
     </div>
