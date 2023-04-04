a617d0c1adb662757391ebb13be895c35a4e5400
parkdh9018 <parkdh9018@naver.com>
2023-04-04 09:42:14 +0900

    [FIX] : 배경 교체를 위한 사전 작업
    - 사전작업
    - 필요없는 파일 삭제(바람개비)

--- frontend/src/components/broadcast/graphicCanvas/GraphicCanvas.tsx
+++ frontend/src/components/broadcast/graphicCanvas/GraphicCanvas.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable */
 import { Canvas, useFrame } from "@react-three/fiber";
-// import { Whirligig } from "./Whirligig";
 import { Cat } from "./Cat";
 import { Background } from "./background/Background";
 import { OrbitControls } from "@react-three/drei";
@@ -8,12 +7,17 @@ import * as THREE from "three";
 
 export const GraphicCanvas = () => {
 
+  const cameraPosition = { fov: 50, near: 0.1, far: 500, position: [-1.34, 0.95, 1.70], scale:1 };
+  const catPosition = new THREE.Vector3(0, 0.1, -0.7);
+  const lightPosition = new THREE.Vector3(0, 1, 0);
+
   return (
-    <Canvas camera={{ fov: 50, near: 0.1, far: 500, position: [-1.34, 0.95, 1.70], scale:1 }}> 
-      <Cat scale={0.5}  position={new THREE.Vector3(0, 0.1, -0.7)}/>
-      <Background />
-      <pointLight intensity={0.3} position={[0,1,0]}/>
+    <Canvas camera={cameraPosition as any}> 
       <ambientLight intensity={0.13} />
+      <Cat scale={0.5}  position={catPosition}/>
+      <Background />
+      <pointLight intensity={0.3} position={lightPosition}/>
+      {/* <OrbitControls/> */}
     </Canvas>
   );
 };
--- frontend/src/components/broadcast/graphicCanvas/background/Background.tsx
+++ frontend/src/components/broadcast/graphicCanvas/background/Background.tsx
@@ -1,23 +1,24 @@
 /* eslint-disable */
-import React, { useRef, useEffect } from 'react';
-import { Canvas, GroupProps, useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
 import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
-import { BackgroundStructure } from './type1/BackgroundStructure';
-import { Objects } from './type1/Objects';
-import { ExtraCat } from './type1/ExtraCat';
+import { BackgroundStructure } from './BackgroundStructure';
+import { Objects } from './Objects';
+import { ExtraCat } from './ExtraCat';
+
 
 interface ExtendedGLTF extends GLTF {
   nodes: any;
 }
 
-export const Background = () => {
+export const Background = ({...props}) => {
+
+  const extraCat_position = new THREE.Vector3(2.6, 0, 1)
 
   return (
     <>
-      <Objects/>
-      <BackgroundStructure/>
-      <ExtraCat scale={0.5} position={new THREE.Vector3(2.6, 0, 1)}/>
+      <Objects num={1}/>
+      <BackgroundStructure num={1}/>
+      <ExtraCat scale={0.5} position={extraCat_position}/>
     </>
   );
 };
