e708f64bfa87cb47546d18bab4c2faf3f7d3358b
2udy <22udy629@gmail.com>
2023-04-05 15:41:20 +0900

    [FIX] About - About 수정 완료

--- frontend/src/pages/home/about/About.tsx
+++ frontend/src/pages/home/about/About.tsx
@@ -1,20 +1,10 @@
 import style from "./About.module.css";
-import AboutImg01 from "@/asset/img/about01.png";
-import AboutImg02 from "@/asset/img/about02.png";
-import AboutImg03 from "@/asset/img/about03.png";
+import AboutImg01 from "@/asset/img/about.png";
 
 export const About = () => {
   return (
     <div className={style.about}>
-      <div>
-        <img src={AboutImg01} className={style.AboutImg}></img>
-      </div>
-      <div>
-        <img src={AboutImg02} className={style.AboutImg}></img>
-      </div>
-      <div>
-        <img src={AboutImg03} className={style.AboutImg}></img>
-      </div>
+      <img src={AboutImg01} className={style.AboutImg}></img>
     </div>
   );
 };
