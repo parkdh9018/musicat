29ce009e9463433593242502fc4a4a54fdf7e09b
parkdh9018 <parkdh9018@naver.com>
2023-04-05 16:57:40 +0900

    [FIX] : 사연 신청 버그 수정

--- frontend/src/connect/axios/queryHooks/story.ts
+++ frontend/src/connect/axios/queryHooks/story.ts
@@ -5,7 +5,6 @@ import { useMutation, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 
 export function storyHook(userSeq: number) {
-
   // 현재 사연 신청 가능한지 체크
   const { data } = useQuery(
     ["storyRequest", userSeq],
@@ -22,21 +21,25 @@ export function storyHook(userSeq: number) {
   );
 
   // 사연 신청
-  const { mutate, isLoading, data : storyReqeustData } = useMutation(
+  const {
+    mutate,
+    isLoading,
+    data: storyReqeustData,
+  } = useMutation(
     async (payload: StoryRequest): Promise<boolean> => {
       if (!payload.storyTitle) {
         useCustomToast("error", "제목을 입력해주세요");
       } else if (
         payload.storyContent.length == 0 ||
-        payload.storyContent.some(v => v.content == "")
+        payload.storyContent.some((v) => v.content == "")
       ) {
         useCustomToast("error", "내용을 확인해주세요");
-      } 
-      else if (payload.storySong.musicTitle == "") {
+      } else if (payload.storySong.musicTitle == "") {
         useCustomToast("error", "신청곡을 확인해주세요");
-      } 
-      else {
-        await $.post("/story", payload);
+      } else {
+        await $.post("/story", payload).catch(() => {
+          useCustomToast("error", "사연을 신청하지 못하였습니다");
+        });
         return true;
       }
 
@@ -47,9 +50,6 @@ export function storyHook(userSeq: number) {
         if (data) {
           useCustomToast("success", "사연이 신청되었습니다");
           return true;
-        } else {
-          useCustomToast("error", "사연을 신청하지 못하였습니다");
-          return false;
         }
       },
     }
